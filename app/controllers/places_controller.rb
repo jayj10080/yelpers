13c11572eb0427fe161719a647c32f8d9dddd34b
jay johnson <jayj10080@gmail.com>
2017-11-18 17:36:48 +0000

    Changed code in controller and index for pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,9 @@
 class PlacesController < ApplicationController
 
 def index
-  @places = Place.all
-end
-
-def index
+  # @places = Place.all
   @places = Place.order("name").page(params[:page]).per_page(5)
 end
-
-
 
 def new
   @place = Place.new
