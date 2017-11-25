a89fc2c3a69036a2478673d186510f9326fea6c1
jay johnson <jayj10080@gmail.com>
2017-11-25 23:31:01 +0000

    adjusted mailer to mail to the user of the place that the comment was added to with a link to the place page

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,10 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@yelpersapp.com"
 
 
-  def comment_added
-    mail(to: "jjcoder1000@gmail.com",
-          subject: "A comment has been added to your place")
-end
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+          subject: "A comment has been added to #{@place.name}")
+  end
 end
