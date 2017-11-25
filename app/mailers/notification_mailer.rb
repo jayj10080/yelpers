957cab7c819f58535a2bbcdb69f6bdf9f6a5561c
jay johnson <jayj10080@gmail.com>
2017-11-25 22:17:27 +0000

    preview email added not working will continue and try again in lesson 44

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,9 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@yelpersapp.com"
 
+
   def comment_added
-    mail(to: "jayj10080@gmail.com",
+    mail(to: "jjcoder1000@gmail.com",
           subject: "A comment has been added to your place")
 end
 end
