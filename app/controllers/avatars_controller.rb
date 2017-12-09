80e466e96d27e288d1377e1a8f4625346bb2fce9
jay johnson <jayj10080@gmail.com>
2017-12-09 21:31:20 +0000

    completed dropdown menu for profile image in navbar having resizing issues with hamburger button

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -26,4 +26,44 @@ end
 
 end
  
+
+ # <% if current_user.avatar.present? %>  
+ #      <%= link_to image_tag(current_user.avatar.avatarpic), user_path(current_user), class: 'profile-pic' %> 
+ #      <% else %>
+ #      <%= link_to image_tag('user.png'), user_path(current_user), class: 'profile-pic' %>
+ #      <% end %>
+
+
+
+
+
+#  <div class="dropdown profile-pic caret">
+
+# <%= image_tag ('user.png'), class: 'dropdown-toggle caret', data: { toggle: 'dropdown' } %>
+#   <span class="caret"></span>
+#   <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+#     <li><a tabindex="-1" href="#">Action</a></li>
+#     <li><a tabindex="-1" href="#">Another action</a></li>
+#     <li><a tabindex="-1" href="#">Something else here</a></li>
+#     <li class="divider"></li>
+#     <li><a tabindex="-1" href="#">Separated link</a></li>
+#   </ul>
+# </div>
+
+
+
+
+# <div class="dropdown profile-pic">
+#     <button class="dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
+#      <%= image_tag 'user.png' %>
+#        <span class="caret"></span>
+#     </button>
+#     <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+#       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Menu item 1</a></li>
+#       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Menu item 2</a></li>
+#       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Menu item 3</a></li>
+#       <li role="presentation" class="divider"></li>
+#       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Menu item 4</a></li>
+#     </ul>
+#   </div>
  
